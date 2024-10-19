6a65566fa09a8231893771964090ab66f1a49f1e
Ryanny <ryannymromero@gmail.com>
2024-10-19 11:25:09 +0800

    Make add to watchlist function

--- src/components/ui/buttons/AddToBtns.jsx
+++ src/components/ui/buttons/AddToBtns.jsx
@@ -7,22 +7,58 @@ import { AuthContext } from '../../../context/AuthContext'
 const AddToBtns = ({ show_id }) => {
   const navigate = useNavigate();
   const { userData, accessToken } = useContext(AuthContext);
-  const BC_URL = import.meta.env.BC_URL;
+  const BC_URL = import.meta.env.VITE_BC_URL;
 
   const isAuth = useCallback(() => {
-    if(!accessToken || !Object.keys(userData).length) {
-      navigate('/login')
+    if(!accessToken) {
+      navigate('/login');
+      return false;
     }
+    return true;
   }, [userData, accessToken, navigate])
   
-  const addToWatchlist = async () => {
 
+  const handleAdd = async () => {
+    if(!isAuth()) return;
+
+    if(!show_id) {
+      alert('Show Id is required');
+      return;
+    }
+
+    try {
+      const response = await fetch(`${BC_URL}/watchlist`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+          user_id: userData?.id,
+          show_id
+        })
+      })
+
+      if(!response.ok) {
+        const errData = await response.json();
+        const errmsg = errData.message || errData.statusText;
+        throw new Error(errmsg);
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+    } catch (error) {
+      console.log(error.message)
+    }
+    
 
   };
+  
 
   return (
     <div className="action_btns flex items-stretch justify-between gap-2">
-      <button onClick={() => addToWatchlist()} className="bg-yellow-600 grow py-2 rounded-lg font-medium text-sm tracking-wide">
+      <button onClick={() => handleAdd()} className="bg-yellow-600 grow py-2 rounded-lg font-medium text-sm tracking-wide">
         Add To Watchlist
       </button>
       <button className="bg-red-700 px-2 rounded-lg">
